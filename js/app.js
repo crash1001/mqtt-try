79bae4b9eaa1a6dc872dc1b87a794ed9841ef102
Ditish Maharjan <ditish.maharjan@rts.com.np>
2018-01-15 09:33:40 +0545

    Login register page and js script done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var emailID="rohit.singh@rts.com.np";
-var url = "https://hardware.wscada.net:483/api/"
 
-function addNewDevice() {
+var url = "https://hardware.wscada.net:483/api/"
+var headerData = {"Content-type" : "application/json"};
+console.log(Cookies.get('emailId'));
+
+function deviceadd() {
     var newDeviceUDI = document.getElementById("deviceUDI").value;
     var targetApi = "deviceadd";
     var apiDeviceAdd = url + targetApi;
@@ -9,45 +11,154 @@ function addNewDevice() {
         "emailId" : emailID,
         "udi" : newDeviceUDI
     });
-    var headerData = {"Content-type" : "application/json"};
-    fetchRequest(headerData, bodyData ,apiDeviceAdd);
+
+    // fetchRequest(headerData, bodyData ,apiDeviceAdd);
+    fetch(url, {
+        method : 'post',
+        headers : headerData,
+        body : bodyData
+    })
+        .then(response => response.json())
+.then(function(data) {
+        console.log('Request succeeded with JSON response', data);
+        if(data.response.errors.length === 0) {
+
+            alert("Device Sucessfully Added");
+        }
+        else
+        {
+            alert("Failed to add device!");
+
+        }
+    })
+        .catch(function(error) {
+            console.log('Request Failed', error);
+        });
 }
 
-function addLinkage() {
+function deviceremove() {
+    var newDeviceUDI = document.getElementById("deviceUDI").value;
+    var targetApi = "deviceremove";
+    var apiDeviceAdd = url + targetApi;
+    var bodyData = JSON.stringify({
+        "emailId" : emailID,
+        "udi" : newDeviceUDI
+    });
+
+    // fetchRequest(headerData, bodyData ,apiDeviceAdd);
+    fetch(url, {
+        method : 'post',
+        headers : headerData,
+        body : bodyData
+    })
+        .then(response => response.json())
+        .then(function(data) {
+        console.log('Request succeeded with JSON response', data);
+        if(data.response.errors.length === 0) {
+
+            alert("Device sucessfully removed");
+        }
+        else
+        {
+            alert("Failed to remove device!");
+
+        }
+    })
+        .catch(function(error) {
+            console.log('Request Failed', error);
+        });
+}
+
+function linkagelink() {
 	var publisherUDIl = document.getElementById("publisherUDI").value;
 	var subscriberUDIl = document.getElementById("subscriberUDI").value;
-	var targetApi = "linkageLink";
+	var targetApi = "linkagelink";
 	var apiDeviceAdd = url + targetApi;
 	var bodyData = JSON.stringify({
 		"emailId" : emailID,
 		"publisherudi" : publisherUDIl,
 		"subscriberudi" : subscriberUDIl
 	});
-	var headerData = {"Content-type" : "application/json"};
-    fetchRequest(headerData, bodyData ,apiDeviceAdd);
+
+    fetch(url, {
+        method : 'post',
+        headers : headerData,
+        body : bodyData
+    })
+        .then(response => response.json())
+.then(function(data) {
+        console.log('Request succeeded with JSON response', data);
+        if(data.response.errors.length === 0) {
+
+            alert("Device Sucessfully Linked");
+        }
+        else
+        {
+            alert("Failed to link device!");
+
+        }
+    })
+        .catch(function(error) {
+            console.log('Request Failed', error);
+        });
+
 }
 
-function fetchRequest(headerData, bodyData, url) {
+function linkageunlink() {
+    var publisherUDIl = document.getElementById("publisherUDI").value;
+    var subscriberUDIl = document.getElementById("subscriberUDI").value;
+    var targetApi = "linkageunlink";
+    var apiDeviceAdd = url + targetApi;
+    var bodyData = JSON.stringify({
+        "emailId" : emailID,
+        "publisherudi" : publisherUDIl,
+        "subscriberudi" : subscriberUDIl
+    });
 
-        fetch(url, {
-            method : 'post',
-            headers : headerData,
-            body : bodyData
-        })
-            .then(response => response.json())
-             .then(function(data) {
-                    console.log('Request succeeded with JSON response', data);
-                    if(data.response.errors.length === 0) {
-                       
-                          alert("OK!");
-                    }
-                   else
-                   {
-                       alert("Failed!");
+    fetch(url, {
+        method : 'post',
+        headers : headerData,
+        body : bodyData
+    })
+        .then(response => response.json())
+.then(function(data) {
+        console.log('Request succeeded with JSON response', data);
+        if(data.response.errors.length === 0) {
 
-                   }
-              })
-            .catch(function(error) {
-                console.log('Request Failed', error);
-            });
+            alert("Device Sucessfully Linked");
+        }
+        else
+        {
+            alert("Failed to link device!");
+
+        }
+    })
+        .catch(function(error) {
+            console.log('Request Failed', error);
+        });
+
 }
+// function fetchRequest(headerData, bodyData, url) {
+//
+//         fetch(url, {
+//             method : 'post',
+//             headers : headerData,
+//             body : bodyData
+//         })
+//             .then(response => response.json())
+//              .then(function(data) {
+//                     console.log('Request succeeded with JSON response', data);
+//                     if(data.response.errors.length === 0) {
+//
+//                           alert("OK!");
+//                     }
+//                    else
+//                    {
+//                        alert("Failed!");
+//
+//                    }
+//               })
+//             .catch(function(error) {
+//                 console.log('Request Failed', error);
+//             });
+// }
--- js/login.js
+++ js/login.js
@@ -1,20 +1,66 @@
+var email;
+var headerData = {"Content-type" : "application/json"};
 
 //validates login credentials
-function validate() {
+function validateLogin() {
 
-        var email = document.getElementById("inputEmail").value;
-        var password = document.getElementById("inputPassword").value;
+        email = $("#inputEmail").val();
+        var password = $("inputPassword").val();
+
         var url ="https://hardware.wscada.net:483/api/userauthenticate";
         var bodyData = JSON.stringify({
             "emailId" : email ,
             "password" : password
         });
-        var headerData = {"Content-type" : "application/json"};
-        fetchRequest(headerData, bodyData, url);
+
+        fetch(url, {
+            method : 'post',
+            headers : headerData,
+            body : bodyData
+        })
+            .then(response => response.json())
+            .then(function(data) {
+            console.log('Request succeeded with JSON response', data);
+            if(data.response.errors.length === 0) {
+                Cookies.set('emailId', email, {expires : 1, path: ''});
+                window.location.replace("index.html");
+            }
+            else
+            {
+                alert("Email address or Password incorrect!!");
+                $("#inputEmail").val(null);
+                $("#inputPassword").val(null);
+
+            }
+        })
+            .catch(function(error) {
+                alert('Request Failed' + error);
+                $("#inputPassword").val(null);
+            });
+
 }
 
+function registerNewUser() {
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var nickName = $("#userName").val();
+    var mobileNumber = $("#mobileNumber").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var country = $("#country").val();
+    var gender = $("input[type='radio'][name='gender']:checked").val();
 
-function fetchRequest(headerData, bodyData, url) {
+    var url ="https://hardware.wscada.net:483/api/userauthenticate";
+    var bodyData = JSON.stringify({
+        "emailId" : email ,
+        "firstName" : firstName,
+        "lastName" : lastName,
+        "nickName" : nickName,
+        "password" : password,
+        "cellPhoneNumber" : mobileNumber,
+        "gender" : gender,
+        "country" : country
+    });
 
     fetch(url, {
         method : 'post',
@@ -22,22 +68,35 @@ function fetchRequest(headerData, bodyData, url) {
         body : bodyData
     })
         .then(response => response.json())
-        .then(function(data) {
-            console.log('Request succeeded with JSON response', data);
-            if(data.response.errors.length === 0) {
-                    window.location.replace("index.html");
-            }
-            else
-            {
-                alert("Email address or Password incorrect!!");
-                document.getElementById("inputEmail").value = null;
-                document.getElementById("inputPassword").value = null;
-            }
-        })
+.then(function(data) {
+        console.log('Request succeeded with JSON response', data);
+        if(data.response.errors.length === 0) {
+            window.location.replace("login.html");
+        }
+        else
+        {
+            alert("Problem while registering, please try again later!");
+            $("firstName").val(firstName);
+            $("lastName").val(lastName);
+            $("userName").val(nickName);
+            $("mobileNumber").val(mobileNumber);
+            $("email").val(email);
+            $("country").val(country);
+            $("password").val(null);
+            $("input[name='gender'].checked").val(gender);
+
+        }
+    })
         .catch(function(error) {
-            console.log('Request Failed', error);
+            alert('Request Failed' + error);
+            $("firstName").val(firstName);
+            $("lastName").val(lastName);
+            $("userName").val(nickName);
+            $("mobileNumber").val(mobileNumber);
+            $("email").val(email);
+            $("country").val(country);
+            $("password").val(null);
+            $("input[name='gender'].checked").val(gender);
         });
 
-
 }
-
