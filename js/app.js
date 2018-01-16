9cc3d1c424e695ed4ea3af04517b4374606990d7
Ditish Maharjan <ditish.maharjan@rts.com.np>
2018-01-16 09:18:06 +0545

    some changes to fetch api - use of request, registration working, login working

--- js/app.js
+++ js/app.js
@@ -1,73 +1,174 @@
-
-var url = "https://hardware.wscada.net:483/api/"
+//Global variables
+var emailId;
 var headerData = {"Content-type" : "application/json"};
-console.log(Cookies.get('emailId'));
+var url = "http://hardware.wscada.net:88/api/";
 
-function deviceadd() {
-    var newDeviceUDI = document.getElementById("deviceUDI").value;
-    var targetApi = "deviceadd";
-    var apiDeviceAdd = url + targetApi;
-    var bodyData = JSON.stringify({
+//Validate Login for Users
+function UserAuthenticate() {
+
+    emailId = $("#inputEmail").val();
+    var password = $("#inputPassword").val();
+    var requestUrl = url + "userauthenticate";
+    var jsonBody = JSON.stringify({
+        "emailId": emailId,
+        "password": password
+    });
+    var request = new Request(requestUrl, {method: 'POST', headers: headerData, body: jsonBody});
+
+    fetch(request)
+        .then(response => response.json())
+        .then(function (responseData) {
+            console.log('data', responseData.response);
+            if (responseData.response.errors.length === 0) {
+                console.log(responseData.response);
+                window.location.replace("web/index.html");
+            }
+            else {
+                alert("Error : " + responseData.response.errors[0].reason);
+                $("inputPassword").val('');
+            }
+        })
+        .catch(function (error) {
+            alert('Request Failed :' + error);
+            $("inputPassword").val(null);
+            $("inputEmail").val(null);
+        });
+}
+//End of function  UserAuthenticate
+
+
+//Adding new users
+function UserCreate() {
+
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+    var nickName = $("#userName").val();
+    var mobileNumber = $("#mobileNumber").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var country = $("#country").val();
+    var gender = $("input[type='radio'][name='gender']:checked").val();
+
+
+    var jsonBody = JSON.stringify({
+        "emailId" : email,
+        "firstName" : firstName,
+        "lastName" : lastName,
+        "nickname" : nickName,
+        "password" : password,
+        "cellPhoneNumber" : mobileNumber,
+        "gender" : gender,
+        "country" : country
+    });
+
+    // var jsonBody = JSON.stringify({
+    //     "emailId" : 'ditish.maharjan@rts.com.np' ,
+    //     "firstName" : 'Ditish',
+    //     "lastName" : 'Maharjan',
+    //     "nickName" : 'dtsmzn',
+    //     "password" : 'password123',
+    //     "cellPhoneNumber" : '9843632323',
+    //     "gender" : 'Male',
+    //     "country" : 'Nepal'
+    // });
+
+    console.log('Body :',jsonBody);
+    var requestUrl = url + "usercreate";
+    var request = new Request(requestUrl, {method: 'POST', headers: headerData, body: jsonBody});
+    console.log('request :', request);
+
+    fetch(request)
+        .then(response => response.json())
+            .then(function (responseData) {
+
+                if (responseData.response.errors.length === 0) {
+                    alert("Sucessfully Registered ");
+                    console.log('response :',responseData.response);
+
+                }
+                else {
+                    alert("Error : " + responseData.response.errors[0].reason);
+                    console.log("E");
+                }
+            })
+        .catch(function (error) {
+            alert('Request Failed :' + error);
+            console.log("fault");
+
+        });
+}
+//End of function UserCreate
+
+//Add Device function
+function DeviceAdd() {
+    var requestUrl = url + "deviceadd";
+    var newDeviceUDI = $("#deviceUDI").val();
+    var jsonBody = JSON.stringify({
         "emailId" : emailID,
         "udi" : newDeviceUDI
     });
+    console.log('Body :',jsonBody);
+    var request = new Request(requestUrl, {method: 'POST', headers: headerData, body: jsonBody});
+    console.log('request :', request);
 
-    // fetchRequest(headerData, bodyData ,apiDeviceAdd);
-    fetch(url, {
-        method : 'post',
-        headers : headerData,
-        body : bodyData
-    })
+    fetch(request)
         .then(response => response.json())
-.then(function(data) {
-        console.log('Request succeeded with JSON response', data);
-        if(data.response.errors.length === 0) {
+.then(function (responseData) {
 
-            alert("Device Sucessfully Added");
+        if (responseData.response.errors.length === 0) {
+            alert("Device Added Sucessfully !! ");
+            console.log('response :',responseData.response);
+
         }
-        else
-        {
-            alert("Failed to add device!");
-
+        else {
+            alert("Error while adding device : " + responseData.response.errors[0].reason);
+            console.log("Error", responseData.response.errors[0].reason);
         }
     })
-        .catch(function(error) {
-            console.log('Request Failed', error);
+        .catch(function (error) {
+            alert('Request Failed :' + error);
+            console.log("fault", error);
+
         });
 }
+//End of Device Add Function
 
-function deviceremove() {
-    var newDeviceUDI = document.getElementById("deviceUDI").value;
-    var targetApi = "deviceremove";
-    var apiDeviceAdd = url + targetApi;
-    var bodyData = JSON.stringify({
+//Remove Device function
+function DeviceRemove() {
+
+    var requestUrl = url + "deviceremove";
+    var newDeviceUDI = $("#deviceUDI").val();
+    var jsonBody = JSON.stringify({
         "emailId" : emailID,
         "udi" : newDeviceUDI
     });
+    console.log('Body :',jsonBody);
+    var request = new Request(requestUrl, {method: 'POST', headers: headerData, body: jsonBody});
+    console.log('request :', request);
 
-    // fetchRequest(headerData, bodyData ,apiDeviceAdd);
-    fetch(url, {
-        method : 'post',
-        headers : headerData,
-        body : bodyData
-    })
+    fetch(request)
         .then(response => response.json())
-        .then(function(data) {
-        console.log('Request succeeded with JSON response', data);
-        if(data.response.errors.length === 0) {
+.then(function (responseData) {
 
-            alert("Device sucessfully removed");
+        if (responseData.response.errors.length === 0) {
+            alert("Device Removed !! ");
+            console.log('response :',responseData.response);
+
         }
-        else
-        {
-            alert("Failed to remove device!");
-
+        else {
+            alert("Error while removing device : " + responseData.response.errors[0].reason);
+            console.log("Error", responseData.response.errors[0].reason);
         }
     })
-        .catch(function(error) {
-            console.log('Request Failed', error);
+        .catch(function (error) {
+            alert('Request Failed :' + error);
+            console.log("fault", error);
+
         });
+
 }
+//End of Remove Device function
+
 
 function linkagelink() {
 	var publisherUDIl = document.getElementById("publisherUDI").value;
--- js/login.js
+++ js/login.js
@@ -1,102 +1,102 @@
-var email;
-var headerData = {"Content-type" : "application/json"};
-
-//validates login credentials
-function validateLogin() {
-
-        email = $("#inputEmail").val();
-        var password = $("inputPassword").val();
-
-        var url ="https://hardware.wscada.net:483/api/userauthenticate";
-        var bodyData = JSON.stringify({
-            "emailId" : email ,
-            "password" : password
-        });
-
-        fetch(url, {
-            method : 'post',
-            headers : headerData,
-            body : bodyData
-        })
-            .then(response => response.json())
-            .then(function(data) {
-            console.log('Request succeeded with JSON response', data);
-            if(data.response.errors.length === 0) {
-                Cookies.set('emailId', email, {expires : 1, path: ''});
-                window.location.replace("index.html");
-            }
-            else
-            {
-                alert("Email address or Password incorrect!!");
-                $("#inputEmail").val(null);
-                $("#inputPassword").val(null);
-
-            }
-        })
-            .catch(function(error) {
-                alert('Request Failed' + error);
-                $("#inputPassword").val(null);
-            });
-
-}
-
-function registerNewUser() {
-    var firstName = $("#firstName").val();
-    var lastName = $("#lastName").val();
-    var nickName = $("#userName").val();
-    var mobileNumber = $("#mobileNumber").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var country = $("#country").val();
-    var gender = $("input[type='radio'][name='gender']:checked").val();
-
-    var url ="https://hardware.wscada.net:483/api/userauthenticate";
-    var bodyData = JSON.stringify({
-        "emailId" : email ,
-        "firstName" : firstName,
-        "lastName" : lastName,
-        "nickName" : nickName,
-        "password" : password,
-        "cellPhoneNumber" : mobileNumber,
-        "gender" : gender,
-        "country" : country
-    });
-
-    fetch(url, {
-        method : 'post',
-        headers : headerData,
-        body : bodyData
-    })
-        .then(response => response.json())
-.then(function(data) {
-        console.log('Request succeeded with JSON response', data);
-        if(data.response.errors.length === 0) {
-            window.location.replace("login.html");
-        }
-        else
-        {
-            alert("Problem while registering, please try again later!");
-            $("firstName").val(firstName);
-            $("lastName").val(lastName);
-            $("userName").val(nickName);
-            $("mobileNumber").val(mobileNumber);
-            $("email").val(email);
-            $("country").val(country);
-            $("password").val(null);
-            $("input[name='gender'].checked").val(gender);
-
-        }
-    })
-        .catch(function(error) {
-            alert('Request Failed' + error);
-            $("firstName").val(firstName);
-            $("lastName").val(lastName);
-            $("userName").val(nickName);
-            $("mobileNumber").val(mobileNumber);
-            $("email").val(email);
-            $("country").val(country);
-            $("password").val(null);
-            $("input[name='gender'].checked").val(gender);
-        });
-
-}
+// var email;
+// var headerData = {"Content-type" : "application/json"};
+//
+// //validates login credentials
+// function validateLogin() {
+//
+//         email = $("#inputEmail").val();
+//         var password = $("inputPassword").val();
+//
+//         var url ="https://hardware.wscada.net:483/api/userauthenticate";
+//         var bodyData = JSON.stringify({
+//             "emailId" : email ,
+//             "password" : password
+//         });
+//
+//         fetch(url, {
+//             method : 'post',
+//             headers : headerData,
+//             body : bodyData
+//         })
+//             .then(response => response.json())
+//             .then(function(data) {
+//             console.log('Request succeeded with JSON response', data);
+//             if(data.response.errors.length === 0) {
+//                 Cookies.set('emailId', email, {expires : 1, path: ''});
+//                 window.location.replace("index.html");
+//             }
+//             else
+//             {
+//                 alert("Email address or Password incorrect!!");
+//                 $("#inputEmail").val(null);
+//                 $("#inputPassword").val(null);
+//
+//             }
+//         })
+//             .catch(function(error) {
+//                 alert('Request Failed' + error);
+//                 $("#inputPassword").val(null);
+//             });
+//
+// }
+//
+// function registerNewUser() {
+//     var firstName = $("#firstName").val();
+//     var lastName = $("#lastName").val();
+//     var nickName = $("#userName").val();
+//     var mobileNumber = $("#mobileNumber").val();
+//     var email = $("#email").val();
+//     var password = $("#password").val();
+//     var country = $("#country").val();
+//     var gender = $("input[type='radio'][name='gender']:checked").val();
+//
+//     var url ="https://hardware.wscada.net:483/api/userauthenticate";
+//     var bodyData = JSON.stringify({
+//         "emailId" : email ,
+//         "firstName" : firstName,
+//         "lastName" : lastName,
+//         "nickName" : nickName,
+//         "password" : password,
+//         "cellPhoneNumber" : mobileNumber,
+//         "gender" : gender,
+//         "country" : country
+//     });
+//
+//     fetch(url, {
+//         method : 'post',
+//         headers : headerData,
+//         body : bodyData
+//     })
+//         .then(response => response.json())
+// .then(function(data) {
+//         console.log('Request succeeded with JSON response', data);
+//         if(data.response.errors.length === 0) {
+//             window.location.replace("login.html");
+//         }
+//         else
+//         {
+//             alert("Problem while registering, please try again later!");
+//             $("firstName").val(firstName);
+//             $("lastName").val(lastName);
+//             $("userName").val(nickName);
+//             $("mobileNumber").val(mobileNumber);
+//             $("email").val(email);
+//             $("country").val(country);
+//             $("password").val(null);
+//             $("input[name='gender'].checked").val(gender);
+//
+//         }
+//     })
+//         .catch(function(error) {
+//             alert('Request Failed' + error);
+//             $("firstName").val(firstName);
+//             $("lastName").val(lastName);
+//             $("userName").val(nickName);
+//             $("mobileNumber").val(mobileNumber);
+//             $("email").val(email);
+//             $("country").val(country);
+//             $("password").val(null);
+//             $("input[name='gender'].checked").val(gender);
+//         });
+//
+// }
